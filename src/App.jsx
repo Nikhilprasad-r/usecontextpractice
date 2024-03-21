e3a270c7e1b3e26a983052ca58e84c8b854a559e
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-21 12:20:54 +0530

    data aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-
+import data from "./assets/product.json";
 function App() {
-  return <></>;
+  return (
+    <>
+      {data.map(() => {
+        return <div>{data.products[0].id}</div>;
+      })}
+    </>
+  );
 }
 
 export default App;
